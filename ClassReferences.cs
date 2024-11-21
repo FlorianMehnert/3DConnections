78b4ba8fd7a551851f74dda923a64178ebcc2555
florian <florian.mehnert@gmail.com>
2024-11-21 10:46:02 +0100

    Initial commit for 3DConnections

---
+++ ClassReferences.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace _3DConnections
+{
+    public class ClassReferences
+    {
+        public List<string> InheritanceReferences { [UsedImplicitly] get; set; }
+        public List<string> FieldReferences { [UsedImplicitly] get; set; }
+        public List<string> MethodReferences { [UsedImplicitly] get; set; }
+
+        public List<string> References => InheritanceReferences.Union(FieldReferences).Union(MethodReferences).ToList();
+    }
+}
---
+++ MainScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _3DConnections
+{
+    public class MainScript : MonoBehaviour
+    {
+        public ScriptVisualization visualizer;
+
+        private void Start()
+        {
+            var classReferences = ClassParser.GetAllClassReferencesParallel("/home/florian/gamedev/my-own-platformer/Assets");
+            visualizer.VisualizeDependencies(classReferences);
+            GetComponent<Button>().onClick.AddListener(TriggerVisualization);
+        }
+
+        private void TriggerVisualization()
+        {
+            var classReferences = ClassParser.GetAllClassReferencesParallel("Assets/Scripts");
+            visualizer.VisualizeDependencies(classReferences);
+        }
+    }
+}
