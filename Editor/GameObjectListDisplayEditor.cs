a2d7905c117f65642a27a9e618f896a49b37f4d4
florian <florian.mehnert@gmail.com>
2025-01-25 18:21:39 +0100

    move node related classes to nodes namespace

--- Editor/GameObjectListDisplayEditor.cs
+++ Editor/GameObjectListDisplayEditor.cs
@@ -1,72 +1,67 @@
-using _3DConnections.Runtime;
+using UnityEngine;
+using UnityEditor;
 
-namespace _3DConnections.Editor
+[CustomEditor(typeof(NodeConnections))]
+public class GameObjectListDisplayEditor : Editor
 {
-    using UnityEngine;
-    using UnityEditor;
-
-    [CustomEditor(typeof(NodeConnections))]
-    public class GameObjectListDisplayEditor : Editor
+    public override void OnInspectorGUI()
     {
-        public override void OnInspectorGUI()
+        var script = (NodeConnections)target;
+
+        // Display the "In Connections"
+        EditorGUILayout.LabelField("In Connections", EditorStyles.boldLabel);
+        if (script.inConnections is { Count: > 0 })
         {
-            var script = (NodeConnections)target;
+            for (var i = 0; i < script.inConnections.Count; i++)
+            {
+                script.inConnections[i] = (GameObject)EditorGUILayout.ObjectField(
+                    $"In {i}",
+                    script.inConnections[i],
+                    typeof(GameObject),
+                    true
+                );
+            }
+        }
+        else
+        {
+            EditorGUILayout.HelpBox("No In Connections defined.", MessageType.Info);
+        }
 
-            // Display the "In Connections"
-            EditorGUILayout.LabelField("In Connections", EditorStyles.boldLabel);
-            if (script.inConnections is { Count: > 0 })
+        EditorGUILayout.Space();
+        // Display the "Out Connections"
+        EditorGUILayout.LabelField("Out Connections", EditorStyles.boldLabel);
+        if (script.outConnections is { Count: > 0 })
+        {
+            for (var i = 0; i < script.outConnections.Count; i++)
             {
-                for (var i = 0; i < script.inConnections.Count; i++)
-                {
-                    script.inConnections[i] = (GameObject)EditorGUILayout.ObjectField(
-                        $"In {i}",
-                        script.inConnections[i],
-                        typeof(GameObject),
-                        true
-                    );
-                }
+                script.outConnections[i] = (GameObject)EditorGUILayout.ObjectField(
+                    $"Out {i}",
+                    script.outConnections[i],
+                    typeof(GameObject),
+                    true
+                );
             }
-            else
-            {
-                EditorGUILayout.HelpBox("No In Connections defined.", MessageType.Info);
-            }
-            EditorGUILayout.Space();
-            // Display the "Out Connections"
-            EditorGUILayout.LabelField("Out Connections", EditorStyles.boldLabel);
-            if (script.outConnections is { Count: > 0 })
-            {
-                for (var i = 0; i < script.outConnections.Count; i++)
-                {
-                    script.outConnections[i] = (GameObject)EditorGUILayout.ObjectField(
-                        $"Out {i}",
-                        script.outConnections[i],
-                        typeof(GameObject),
-                        true
-                    );
-                }
-            }
-            else
-            {
-                EditorGUILayout.HelpBox("No Out Connections defined.", MessageType.Info);
-            }
+        }
+        else
+        {
+            EditorGUILayout.HelpBox("No Out Connections defined.", MessageType.Info);
+        }
 
-            // Optionally add buttons for managing the lists
-            EditorGUILayout.Space();
-            if (GUILayout.Button("Add In Connection"))
-            {
-                script.inConnections?.Add(null);
-            }
+        // Optionally add buttons for managing the lists
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Add In Connection"))
+        {
+            script.inConnections?.Add(null);
+        }
 
-            if (GUILayout.Button("Add Out Connection"))
-            {
-                script.outConnections?.Add(null);
+        if (GUILayout.Button("Add Out Connection"))
+        {
+            script.outConnections?.Add(null);
+        }
 
-            }
-
-            if (GUI.changed)
-            {
-                EditorUtility.SetDirty(script);
-            }
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(script);
         }
     }
 }
--- Runtime/Nodes/NodeConnection.cs
+++ Runtime/Nodes/NodeConnection.cs
@@ -1,14 +1,11 @@
 using UnityEngine;
 
-namespace _3DConnections.Runtime
+[System.Serializable]
+public class NodeConnection
 {
-    [System.Serializable]
-    public class NodeConnection
-    {
-        public GameObject startNode;
-        public GameObject endNode;
-        public LineRenderer lineRenderer;
-        public Color connectionColor = new(1,255,50);
-        public float lineWidth = 0.1f;
-    }
+    public GameObject startNode;
+    public GameObject endNode;
+    public LineRenderer lineRenderer;
+    public Color connectionColor = new(1, 255, 50);
+    public float lineWidth = 0.1f;
 }
