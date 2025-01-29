364190bb24d7e36366d5e4bec674fe8882ccd96b
florian <florian.mehnert@gmail.com>
2025-01-29 20:16:32 +0100

    add fpsdisplay as child to manager

---
+++ Runtime/GUI/FPSDisplay.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class FPSDisplay : MonoBehaviour
+{
+    private float _deltaTime;
+
+    private void Update()
+    {
+        _deltaTime += (Time.unscaledDeltaTime - _deltaTime) * 0.1f;
+    }
+
+    private void OnGUI()
+    {
+        int w = Screen.width, h = Screen.height;
+
+        var style = new GUIStyle();
+        var rect = new Rect(w - 100, 10, 90, 30); // Adjust position
+
+        style.alignment = TextAnchor.UpperRight;
+        style.fontSize = h / 30;
+        style.normal.textColor = Color.white;
+
+        var fps = 1.0f / _deltaTime;
+        var text = $"FPS: {fps:F1}";
+
+        GUI.Label(rect, text, style);
+    }
+}
