15165fd9ad09aebe3601cf370c1eca878a6f6ee6
florian <florian.mehnert@gmail.com>
2025-05-10 20:57:14 +0200

    add description to artificialGameObject class

--- Runtime/Managers/ArtificialGameObject.cs
+++ Runtime/Managers/ArtificialGameObject.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+/// <summary>
+/// Useful component to distinguish nodes from the gameObjects they represent
+/// </summary>
 public class ArtificialGameObject : MonoBehaviour
 {
 }
