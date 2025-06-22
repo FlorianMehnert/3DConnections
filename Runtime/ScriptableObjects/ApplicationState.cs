74dcc2d3fe3951eb185fa231ac04c6da10cd2826
florian <florian.mehnert@gmail.com>
2025-06-22 15:28:41 +0200

    add docstring to application state

--- Runtime/ScriptableObjects/ApplicationState.cs
+++ Runtime/ScriptableObjects/ApplicationState.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ApplicationState", menuName = "3DConnections/ScriptableObjects/ApplicationState", order = 1)]
 public class ApplicationState : ScriptableObject
 {
+    /// <summary>
+    /// state management -> set this variable if any nodes are on the field and only set to false in clear
+    /// </summary>
     public bool spawnedNodes;
 }
