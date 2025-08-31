b243d6285751ae8d22daca1b675d97fb1d87d1ab
Florian Mehnert <florian.mehnert@gmail.com>
2025-08-31 12:17:58 +0200

    add simulation parameters

---
+++ Runtime/ScriptableObjects/SimulationParameters.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _3DConnections.Runtime.ScriptableObjects
+{
+    [Tooltip("Holds the current simulation type")]
+    [CreateAssetMenu(fileName = "Simulation Parameters",
+        menuName = "3DConnections/ScriptableObjects/Simulation Parameters", order = 1)]
+    public class SimulationParameters : ScriptableObject
+    {
+        public SimulationType simulationType = SimulationType.Static;
+    }
+}
