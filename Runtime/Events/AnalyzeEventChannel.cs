0068eabdf69467d9cc0aee848074b4bcec09b78f
Florian Mehnert <florian.mehnert@gmail.com>
2025-08-31 12:17:28 +0200

    rename analyze event ot analyzeEventChannel

---
+++ Runtime/Events/AnalyzeEventChannel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace _3DConnections.Runtime.Events
+{
+    [Tooltip("Holds all events that are triggered by the Analyze Scene button")]
+    [CreateAssetMenu(fileName = "AnalyzeEventChannel", menuName = "3DConnections/Events/Analyze Scene Event Channel")]
+    public class AnalyzeEventChannel : ScriptableObject
+    {
+        public LayoutEvent layoutEvent;
+        public SimulationEvent simulationEvent;
+
+        public void TriggerEvent()
+        {
+            layoutEvent.TriggerEvent();
+            simulationEvent.TriggerEvent();
+        }
+    }
+}
