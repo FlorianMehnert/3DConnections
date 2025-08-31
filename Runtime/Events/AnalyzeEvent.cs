9a1571250e81b7a87861c34401ffc68ca321f0c6
Florian Mehnert <florian.mehnert@gmail.com>
2025-08-31 11:01:28 +0200

    add analyze event

---
+++ Runtime/Events/AnalyzeEvent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+namespace _3DConnections.Runtime.Events
+{
+    [CreateAssetMenu(fileName = "AnalyzeEvent", menuName = "3DConnections/Events/Analyze Scene Event")]
+    public class AnalyzeEvent : ScriptableObject
+    {
+        // only this class can raise it
+        private event Action OnEventTriggered;
+
+        /// <summary>
+        /// Raises the event, notifying all listeners
+        /// </summary>
+        public void Raise()
+        {
+            OnEventTriggered?.Invoke();
+        }
+
+        /// <summary>
+        /// Subscribe a listener to this event
+        /// </summary>
+        public void AddListener(Action listener)
+        {
+            OnEventTriggered += listener;
+        }
+
+        /// <summary>
+        /// Unsubscribe a listener from this event
+        /// </summary>
+        public void RemoveListener(Action listener)
+        {
+            OnEventTriggered -= listener;
+        }
+
+        /// <summary>
+        /// Clears all listeners (useful when domain reload is disabled)
+        /// </summary>
+        public void Clear()
+        {
+            OnEventTriggered = null;
+        }
+
+        private void OnEnable()
+        {
+#if UNITY_EDITOR
+            // Clear listeners when entering play mode to avoid duplicates
+            // when domain reload is disabled
+            if (!Application.isPlaying)
+            {
+                OnEventTriggered = null;
+            }
+#endif
+        }
+    }
+}
