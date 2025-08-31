6155907075ded66d91d3998f715ceda7351f6991
Florian Mehnert <florian.mehnert@gmail.com>
2025-08-31 12:17:44 +0200

    add layout and simulation events

---
+++ Runtime/Events/LayoutEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace _3DConnections.Runtime.Events
+{
+    [CreateAssetMenu(fileName = "LayoutEvent", menuName = "3DConnections/Events/Layout Nodes Event")]
+    public class LayoutEvent : ScriptableObject
+    {
+        public UnityEvent onEventTriggered;
+
+        public void TriggerEvent()
+        {
+            onEventTriggered?.Invoke();
+        }
+    }
+}
---
+++ Runtime/Events/SimulationEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace _3DConnections.Runtime.Events
+{
+    [CreateAssetMenu(fileName = "SimulationEvent", menuName = "3DConnections/Events/Simulate Nodes Event")]
+    public class SimulationEvent : ScriptableObject
+    {
+        public UnityEvent onEventTriggered;
+
+        public void TriggerEvent()
+        {
+            onEventTriggered?.Invoke();
+        }
+    }
+}
