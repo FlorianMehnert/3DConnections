80e7c64e86d44ff075c8cf368e29ca2cb4688511
Florian Mehnert <florian.mehnert@gmail.com>
2025-09-04 12:46:15 +0200

    add reset parameters function for layout parameters

---
+++ Editor/LayoutParametersEditor.cs
@@ -0,0 +1,21 @@
+﻿using _3DConnections.Runtime.ScriptableObjects;
+using UnityEditor;
+using UnityEngine;
+
+namespace _3DConnections.Editor
+{
+    [CustomEditor(typeof(LayoutParameters))] // no [CanEditMultipleObjects]
+    public class LayoutParametersEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var parameters = (LayoutParameters)target;
+            if (GUILayout.Button("Reset All Parameters"))
+            {
+                parameters.ResetToDefaultParameters();
+            }
+        }
+    }
+}
--- Runtime/ScriptableObjects/LayoutParameters.cs
+++ Runtime/ScriptableObjects/LayoutParameters.cs
@@ -18,9 +18,8 @@ namespace _3DConnections.Runtime.ScriptableObjects
         public float levelSpacing = 10f;
         public float nodeSpacing = 2f;
         public float subtreeSpacing = 2f;
-        
-        [Header("GRIP")]
-        public float attractionStrength = 1f;
+
+        [Header("GRIP")] public float attractionStrength = 1f;
         public float repulsion = 100f;
         public float idealEdgeLength = 5f;
         public int iterations = 50;
@@ -28,5 +27,39 @@ namespace _3DConnections.Runtime.ScriptableObjects
         public float initialTemperature = 10.0f;
         public float coarseningLevels = 3f;
         public float coarseningRatio = 0.5f;
+
+        [Header("FM³")] public bool isDirected = true;
+        public float directionalForce = 2f;
+        public float cycleHandlingStrength = 0.5f;
+        public bool useBarnesHut = true;
+
+
+        public void ResetToDefaultParameters()
+        {
+            Debug.Log("resetting layout parameters to default parameters");
+            minDistance = 2f;
+            startRadius = 3f;
+            radiusInc = 4f;
+            rootSpacing = 10f;
+
+            levelSpacing = 10f;
+            nodeSpacing = 2f;
+            subtreeSpacing = 2f;
+            
+            isDirected = true;
+
+            attractionStrength = 1f;
+            repulsion = 100f;
+            idealEdgeLength = 5f;
+            iterations = 50;
+            coolingFactor = 0.95f;
+            initialTemperature = 10.0f;
+            coarseningLevels = 3f;
+            coarseningRatio = 0.5f;
+
+            directionalForce = 2f;
+            cycleHandlingStrength = 0.5f;
+            useBarnesHut = true;
+        }
     }
 }
