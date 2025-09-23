4c5efc0bc04054652334da5881a58b4bd04a85b5
Florian Mehnert <florian.mehnert@gmail.com>
2025-09-23 15:42:24 +0200

    make menu screenshot similar to nodegraph screenshot

--- Runtime/GUI/MenuScreenshot.cs
+++ Runtime/GUI/MenuScreenshot.cs
@@ -9,18 +9,28 @@
     {
         public RenderTexture renderTexture;
 
+        public Camera uiCamera;
+
         [ContextMenu("Capture Menu")]
         private void CaptureMenu()
         {
+            // render UI camera into RT
+            uiCamera.targetTexture = renderTexture;
+            uiCamera.Render();
+
             var tex = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGBA32, false);
             RenderTexture.active = renderTexture;
             tex.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
             tex.Apply();
+
+            // cleanup
+            uiCamera.targetTexture = null;
             RenderTexture.active = null;
 
             var bytes = tex.EncodeToPNG();
             System.IO.File.WriteAllBytes(Application.dataPath + "/../menu.png", bytes);
             Debug.Log("Saved menu.png");
         }
+
     }
 }
